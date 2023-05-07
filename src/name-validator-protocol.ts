bcd3064cb29055a2aaba6d209cfabaa399aeae65
Barthmosz <126964783+Barthmosz@users.noreply.github.com>
2023-05-07 01:06:45 -0300

    test: ensure NameValidator returns false if an invalid name is provided

---
+++ src/name-validator-protocol.ts
@@ -0,0 +1,3 @@
+export interface Validator {
+    validate: (name: string) => boolean
+}
---
+++ src/name-validator.spec.ts
@@ -0,0 +1,14 @@
+import { NameValidator } from './name-validator'
+import { type Validator } from './name-validator-protocol'
+
+const makeSut = (): Validator => {
+    const sut = NameValidator
+    return sut
+}
+
+describe('Name Validator', () => {
+    test('should return false if an invalid name is provided', () => {
+        const sut = makeSut()
+        expect(sut.validate('Joe#1')).toBe(false)
+    })
+})
