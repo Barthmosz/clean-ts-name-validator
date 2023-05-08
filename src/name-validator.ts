31c10b0cc966ec256f11a70b86f1ce486be8b2cc
Barthmosz <126964783+Barthmosz@users.noreply.github.com>
2023-05-07 23:48:16 -0300

    docs: add jsdocs to NameValidator

--- src/name-validator.ts
+++ src/name-validator.ts
@@ -1,4 +1,11 @@
 export class NameValidator {
+    /**
+     * Validates a name.
+     * @param {string} name
+     * @param {number} minLength
+     * @param {number} maxLength
+     * @returns {boolean}
+    */
     static validate(name: string, minLength: number = 3, maxLength: number = 12): boolean {
         return new RegExp(`^[A-za-z]{${minLength},${maxLength}}$`).test(name)
     }
