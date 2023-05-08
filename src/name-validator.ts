ab23eb8f7e91f7aa785c1fc9dc6c80c5e333297d
Barthmosz <126964783+Barthmosz@users.noreply.github.com>
2023-05-08 00:05:24 -0300

    docs: add description docs to NameValidator

--- src/name-validator.ts
+++ src/name-validator.ts
@@ -1,6 +1,7 @@
 export class NameValidator {
     /**
-     * Validates a name.
+     * @summary Validates a name.
+     * @description The name cannot contain numbers or special characters, and it must be between the minimum and maximum value, which can be passed in the second and third parameter, respectively.
      * @param {string} name
      * @param {number} minLength
      * @param {number} maxLength
